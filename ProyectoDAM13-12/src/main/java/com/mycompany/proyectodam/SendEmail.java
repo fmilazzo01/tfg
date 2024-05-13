4661ea177ec7f10fa418976d3f6449570df55660
fmilazzo01 <122482353+fmilazzo01@users.noreply.github.com>
2024-05-13 23:54:45 +0200

    Update

--- ProyectoDAM13-12/src/main/java/com/mycompany/proyectodam/SendEmail.java
+++ ProyectoDAM13-12/src/main/java/com/mycompany/proyectodam/SendEmail.java
@@ -9,10 +9,10 @@ public class SendEmail {
     public static void main(String[] args) {
         // Construir el correo electrónico
         Email email = EmailBuilder.startingBlank()
-                .from("GymAPP", "thegymapp@outlook.com") // ACA FROM
-                .to("Franqui Milazzo", "franquimilazzo@icloud.com") //
+              //  .from("GymAPP", "thegymapp@outlook.com") // no funciona
+                .to("NOMBRE DESTINATARIO", "franquimilazzo@icloud.com") //
                 .withSubject("Tu nueva rutina para el gimnasio!!")
-                .withPlainText("Hola Franqui,\n\nAquí tienes tu nueva rutina de gimnasio:\n\n1. Calentamiento: 10 minutos de cardio ligero.\n2. Pesas: 3 series de 10 repeticiones de press de banca.\n3. Cardio: 20 minutos de bicicleta estática.\n4. Estiramiento: 10 minutos de estiramientos.\n\n¡Espero que te resulte útil!\n\nSaludos,\nTu nombre")
+                .withPlainText("mensaje aca")
                 .buildEmail();
 
         // Configurar el servidor SMTP
